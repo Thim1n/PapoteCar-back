2e5d3aa45f939d0964b8cf4964da4dfc8150fe21
Thim1n <Thibaudpubert85@gmail.com>
2026-03-09 12:35:36 +0100

    Spring Initializer

---
+++ src/main/java/com/PapoteCar/PapoteCar/PapoteCarApplication.java
@@ -0,0 +1,13 @@
+package com.PapoteCar.PapoteCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapoteCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapoteCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PapoteCar/PapoteCar/PapoteCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PapoteCar.PapoteCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapoteCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
